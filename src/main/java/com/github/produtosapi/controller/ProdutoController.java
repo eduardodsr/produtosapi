66b6098735d3be2595f6833dddb9b38d7213eda3
Eduardo Rodrigues <eduardodsr@gmail.com>
2025-03-02 15:17:22 -0300

    Implement CRUD operations for Produto entity in controller and repository

--- src/main/java/com/github/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/github/produtosapi/controller/ProdutoController.java
@@ -2,11 +2,10 @@ package com.github.produtosapi.controller;
 
 import com.github.produtosapi.model.Produto;
 import com.github.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 // controller = API Rest
@@ -31,4 +30,28 @@ public class ProdutoController {
         produtoRepository.save(produto);
         return produto;
     }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+//        Optional<Produto> produto = produtoRepository.findById(id);
+//        return produto.isPresent() ? produto.get() : null;
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping()
+    public List<Produto> buscar(@RequestParam(value = "nome") String nome) {
+        return produtoRepository.findByNome(nome);
+    }
+
 }
--- src/main/java/com/github/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/com/github/produtosapi/repository/ProdutoRepository.java
@@ -3,7 +3,11 @@ package com.github.produtosapi.repository;
 import com.github.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// repository = Classes requisicoes Banco de Dados (h2 console)
+import java.util.List;
+
+// repository = Requisicoes ao Banco de Dados (h2 console)
 
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
 }
